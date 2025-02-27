8dc6e164c01a6abf358247b25b8faa0277acd6f7
Bryan Gordon <bryanbge92@gmail.com>
2025-02-27 00:37:06 -0500

    Create component

---
+++ src/lib/Movie.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let poster, title, year
+</script>
+
+<article>
+  <img src={poster} alt={title}>
+  <h3>{title}</h3>
+  <span>{year}</span>
+  {#if 2024 - year < 5}
+    <strong>Pelicula nueva</strong>
+  {/if}
+</article>
+
+<style>
+  article {
+    border: 1px solid #eee;
+    border-radius: 4px;
+    margin-bottom: 8px;
+    padding: 16px;
+  }
+
+  h3 {
+    font-size: 24px;
+    margin-bottom: 4px;
+  }
+</style>
